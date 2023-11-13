0fd7933eb2847ea16e6f295d397da4d9f48592ca
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-11-14 00:59:10 +0100

    Destructering the source

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,12 +1,10 @@
 import {inventory, bestSellingTv} from "../constants/inventory.js";
 export const {type, name, price} = inventory;
-export const {sourceImg} = bestSellingTv;
-
+export const {sourceImg, availableSizes, sold: bestSell, brand, name: nameBestTv, type: bestType, screenQuality} = bestSellingTv;
 export {bestSellingTv, inventory};
 //total inventory check
 export let totalInventorySold = 0;
-//total sold
-export let bestSell = bestSellingTv.sold;
+
 //forEach to check total inventory
 inventory.forEach(item => totalInventorySold += item.sold);
 export let totalInventory = 0;
@@ -15,21 +13,17 @@ for (let index in inventory) {
 }
 // totalSold include inventory check
 export let totalSold = totalInventorySold + bestSell;
-
 //total available for sale
 export let totalForSale = totalInventory - totalSold + bestSell;
 // export default ProductSalesCalculator;
 
-
-
 // Opdracht 2
 //Productweergave tv
-export let tvBrand = `${bestSellingTv.brand} ${bestSellingTv.name} ${bestSellingTv.type} - ${bestSellingTv.screenQuality}`;
+export let tvBrand = `${brand} ${nameBestTv} ${bestType} - ${screenQuality}`;
 
 //Tv inc cm weergave
-
 export const tvSizes = (index) => {
-    const inches = bestSellingTv.availableSizes[index];
+    const inches = availableSizes[index];
     const centimeters = inches * 2.54;
     return `${inches} inch (${centimeters} cm) |`;
 }
